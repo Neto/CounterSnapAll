f4c7663e62afb4d66bf81b426e9c87069fe8217b
Mentor Neto <neto@bullet.com.br>
2022-06-29 16:46:15 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+from datetime import datetime
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+
+  # Connect to the database
+  con = sqlite3.connect('followers.db')
+  cur = con.cursor()
+
+  # Create an empty list for the records
+  records = []
+
+  try:
+
+    # Get all records
+    all_stats = cur.execute('SELECT * FROM monthly_stats ORDER BY date DESC').fetchall()
+
+    # Create an object ("dictionary" in Python) for each record
+    for item in all_stats:
+      dt = datetime.fromtimestamp(item[0])
+
+      record = {
+        'date': dt.strftime('%-d %b %H:%M'), # Format the date
+        'emailC': item[1],
+        'youtube': item[2],
+        'youtubeL': item[3],
+        'twitter': item[4],
+        'instagram': item[5],
+        'twitterL': item[6],
+        'instagramL': item[7],
+        'total': item[8]
+      }
+      
+      # Add each record object to the list
+      records.append(record)
+    
+  except Exception as e:
+    pass
+  
+  # Disconnect from the database
+  con.close()
+
+  # Pass the list of objects to the template for rendering
+  return render_template('stats.html', records=records)
