b6a7f56f50233058b61d0fcb9ece2022007b8483
Mentor Neto <neto@spinoff.digital>
2022-07-11 01:15:24 -0300

    commit on master

--- get_stats.py
+++ get_stats.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from datetime import datetime
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 import sqlite3
 import requests
@@ -57,80 +58,98 @@ driver.implicitly_wait(10)
 #BRASIL
 
 #EMail
+#cookies = {
+#    'NPS_efd9b1a6_last_seen': '1657081628591',
+#    'intercom-id-hq3qq4eb': 'e5043f4d-96ed-4f4e-b632-499a74d25eb2',
+#    'drift_aid': 'a0fe3ffd-3efa-4b45-8109-155e76b47eb0',
+#    'driftt_aid': 'a0fe3ffd-3efa-4b45-8109-155e76b47eb0',
+#    '_hjSessionUser_863034': 'eyJpZCI6ImE3NDdhZTkwLTc4MjgtNTUyZS04NGFjLWM4YTM2OTIyM2M3OSIsImNyZWF0ZWQiOjE2NTcwODE2Mjc3MjYsImV4aXN0aW5nIjp0cnVlfQ==',
+#    '_hjSessionUser_2716062': 'eyJpZCI6IjY4MzkzNTZkLWYwYzItNWZlZi04NjA5LTE0ZDYyYjlmYzA4NSIsImNyZWF0ZWQiOjE2NTcwODE1OTU3NzAsImV4aXN0aW5nIjp0cnVlfQ==',
+#    'split': '%7B%22xp_ab_test%22%3A%22variant%22%2C%22caterpie_activation_strategies%22%3A%22control%22%7D',
+#    'ajs_group_id': '434927',
+#    'ajs_user_id': '697398',
+#    'ajs_anonymous_id': '2e1316f6-da5f-4f8b-bbd3-cdf64038e530',
+#    'cto_bundle': 'F9oZN19MS05nTzFUYVB4NmI5NFRWaU56Vk5oRmZuNkVsRXdQbWpVaVQ0WEFkekdpS0FQMzNpaDcwbGdheDIyRSUyRkRmZlluVEwzOEg0QWtRa0c0TkVud3hkeWdHR09CZFhCenFRaWlqbDlnU2MyVUYlMkJEVHZKNEdBOFJDJTJCN0RaM1Y3RWpBcXFIRG16QXpleHVPdkk4cW9BTUQzVFVuRUtGWTlsMHRQNTM2aFlhcGYxcTFEejEyTDVOTzQ4Nm1kZ3NocklaWnV5ejBISVRTRSUyQnVaaWpaSTdMRmdDWXclM0QlM0Q',
+#    'NPS_efd9b1a6_surveyed': 'true',
+#    '_hjCachedUserAttributes': 'eyJhdHRyaWJ1dGVzIjp7IkNvdW50cnlfSVBzdGFjayI6IkJSIiwiYWNjb3VudF9pZCI6IjQzNDkyNyIsImNvdW50cnlfY29kZSI6IkJSIiwiZW1haWwiOiJuZXRvQHNwaW5vZmYuZGlnaXRhbCIsImxhbmd1YWdlIjoicHQtQlIiLCJwbGFuIjoiUHJvIiwicm9sZSI6IkFETUlOIn0sInVzZXJJZCI6IjY5NzM5OCJ9',
+#    'drift_eid': '697398',
+#    '_uetvid': 'e5ef5cc0fce311ecac2e85d56bbe46fb',
+#    'intercom-session-hq3qq4eb': 'eXlITWZaWTh6bHZ6VnlUQVJ0MWlYSFVxcnJmaDFncW84UG4yYWhCWU9HM0t0Q3ZSaC9LZnNXdlp1aHhVRngyUi0tZUVsMXNHL05rYW05d1gxUVcxTityZz09--9ccbab8fffbe3b59cfe15d7c9f5c2c220b67a917',
+#    '__rdsid': 'ccfa80beeec16ead174e9fdbbad5a8df',
+#    'ak_bmsc': 'A8870C800FAC5A2CF39AEBFE8589E270~000000000000000000000000000000~YAAQT9PPF1QbWLSBAQAABM286RCOZfDhUhGGmxotyMQGWIw89j4tMflmZ3rNcr4F9ikEKeEar7JLAsMo0YhSC8AvW8lolv8lGsCoLWohfS5OWRwDK316WfCqu7rLH+hiMVbSco+6OSUY2fq1+QLJux5TCvUrddK4OOXVR+FVelgT33kGjNvhBeAJEJg4+QCBsuh6HvQeYNkVcLzSazDfrOhSVs/3Gm4KRK1yIuMSUzm9GOCIWA4alE4cnbxHx1gUjfpEQUCUtzqA1zOmJyTzLgd6/DDuRNUpGVdWYl3YdM/bE89AFBGRI0ftLhBIumPlgUfcnPw0Zk3XhL9cQkdbAhvxSHSUrgbE/jYY1BZpQOs5tSIfWD6U/hmZQVVCu7AMfPboGQ5LniyWqAry87yKTQ==',
+#    '_dd_s': '',
+#    '_hjSession_2716062': 'eyJpZCI6ImEzNDlmZDI4LTc5Y2YtNGM1My1iZmIyLWE0ZWMwYjZkZWNhNSIsImNyZWF0ZWQiOjE2NTc0ODM4NzQ1NTksImluU2FtcGxlIjpmYWxzZX0=',
+#    '_hjAbsoluteSessionInProgress': '0',
+#    '_ga_GYGJG4ZD5K': 'GS1.1.1657483874.9.0.1657483874.0',
+#    '_ga': 'GA1.3.1486625735.1657081595',
+#    '_gid': 'GA1.3.394741834.1657483875',
+#    '_gac_UA-17276574-1': '1.1657483875.%2528not%2520set%2529',
+#    '_gat_UA-17276574-1': '1',
+#    'bm_sv': 'C043D409463DF3BE15D8314B2E3C3C6B~YAAQT9PPF+4nWLSBAQAAMFe96RDd3Py7GaOl7jnq4l++yXhCBvmWib049/SmiNSl/9jD06CwM77vpYALxhMBYlbQnNlHPvjW276uAxkYzEZ5o1TTKJy+DZ5nWgio4dHhYX/5+g7KipUmpB5PbtBCk10FULOZDJEopPHLz69Kc8Hq35LotJIUoHDZMs3cM2cmjLzyaL9v/1dmvZ2ZcpbYm5FgP7nITd+y8FvgdTrU5MqDyxIRVj3bLud2SfEYHuVPd/PMrGBu~1',
+#}
+#
+#headers = {
+#    'authority': 'app.rdstation.com.br',
+#    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+#    'accept-language': 'en-US,en;q=0.9',
+#    # Requests sorts cookies= alphabetically
+#    # 'cookie': 'NPS_efd9b1a6_last_seen=1657081628591; intercom-id-hq3qq4eb=e5043f4d-96ed-4f4e-b632-499a74d25eb2; drift_aid=a0fe3ffd-3efa-4b45-8109-155e76b47eb0; driftt_aid=a0fe3ffd-3efa-4b45-8109-155e76b47eb0; _hjSessionUser_863034=eyJpZCI6ImE3NDdhZTkwLTc4MjgtNTUyZS04NGFjLWM4YTM2OTIyM2M3OSIsImNyZWF0ZWQiOjE2NTcwODE2Mjc3MjYsImV4aXN0aW5nIjp0cnVlfQ==; _hjSessionUser_2716062=eyJpZCI6IjY4MzkzNTZkLWYwYzItNWZlZi04NjA5LTE0ZDYyYjlmYzA4NSIsImNyZWF0ZWQiOjE2NTcwODE1OTU3NzAsImV4aXN0aW5nIjp0cnVlfQ==; split=%7B%22xp_ab_test%22%3A%22variant%22%2C%22caterpie_activation_strategies%22%3A%22control%22%7D; ajs_group_id=434927; ajs_user_id=697398; ajs_anonymous_id=2e1316f6-da5f-4f8b-bbd3-cdf64038e530; cto_bundle=F9oZN19MS05nTzFUYVB4NmI5NFRWaU56Vk5oRmZuNkVsRXdQbWpVaVQ0WEFkekdpS0FQMzNpaDcwbGdheDIyRSUyRkRmZlluVEwzOEg0QWtRa0c0TkVud3hkeWdHR09CZFhCenFRaWlqbDlnU2MyVUYlMkJEVHZKNEdBOFJDJTJCN0RaM1Y3RWpBcXFIRG16QXpleHVPdkk4cW9BTUQzVFVuRUtGWTlsMHRQNTM2aFlhcGYxcTFEejEyTDVOTzQ4Nm1kZ3NocklaWnV5ejBISVRTRSUyQnVaaWpaSTdMRmdDWXclM0QlM0Q; NPS_efd9b1a6_surveyed=true; _hjCachedUserAttributes=eyJhdHRyaWJ1dGVzIjp7IkNvdW50cnlfSVBzdGFjayI6IkJSIiwiYWNjb3VudF9pZCI6IjQzNDkyNyIsImNvdW50cnlfY29kZSI6IkJSIiwiZW1haWwiOiJuZXRvQHNwaW5vZmYuZGlnaXRhbCIsImxhbmd1YWdlIjoicHQtQlIiLCJwbGFuIjoiUHJvIiwicm9sZSI6IkFETUlOIn0sInVzZXJJZCI6IjY5NzM5OCJ9; drift_eid=697398; _uetvid=e5ef5cc0fce311ecac2e85d56bbe46fb; intercom-session-hq3qq4eb=eXlITWZaWTh6bHZ6VnlUQVJ0MWlYSFVxcnJmaDFncW84UG4yYWhCWU9HM0t0Q3ZSaC9LZnNXdlp1aHhVRngyUi0tZUVsMXNHL05rYW05d1gxUVcxTityZz09--9ccbab8fffbe3b59cfe15d7c9f5c2c220b67a917; __rdsid=ccfa80beeec16ead174e9fdbbad5a8df; ak_bmsc=A8870C800FAC5A2CF39AEBFE8589E270~000000000000000000000000000000~YAAQT9PPF1QbWLSBAQAABM286RCOZfDhUhGGmxotyMQGWIw89j4tMflmZ3rNcr4F9ikEKeEar7JLAsMo0YhSC8AvW8lolv8lGsCoLWohfS5OWRwDK316WfCqu7rLH+hiMVbSco+6OSUY2fq1+QLJux5TCvUrddK4OOXVR+FVelgT33kGjNvhBeAJEJg4+QCBsuh6HvQeYNkVcLzSazDfrOhSVs/3Gm4KRK1yIuMSUzm9GOCIWA4alE4cnbxHx1gUjfpEQUCUtzqA1zOmJyTzLgd6/DDuRNUpGVdWYl3YdM/bE89AFBGRI0ftLhBIumPlgUfcnPw0Zk3XhL9cQkdbAhvxSHSUrgbE/jYY1BZpQOs5tSIfWD6U/hmZQVVCu7AMfPboGQ5LniyWqAry87yKTQ==; _dd_s=; _hjSession_2716062=eyJpZCI6ImEzNDlmZDI4LTc5Y2YtNGM1My1iZmIyLWE0ZWMwYjZkZWNhNSIsImNyZWF0ZWQiOjE2NTc0ODM4NzQ1NTksImluU2FtcGxlIjpmYWxzZX0=; _hjAbsoluteSessionInProgress=0; _ga_GYGJG4ZD5K=GS1.1.1657483874.9.0.1657483874.0; _ga=GA1.3.1486625735.1657081595; _gid=GA1.3.394741834.1657483875; _gac_UA-17276574-1=1.1657483875.%2528not%2520set%2529; _gat_UA-17276574-1=1; bm_sv=C043D409463DF3BE15D8314B2E3C3C6B~YAAQT9PPF+4nWLSBAQAAMFe96RDd3Py7GaOl7jnq4l++yXhCBvmWib049/SmiNSl/9jD06CwM77vpYALxhMBYlbQnNlHPvjW276uAxkYzEZ5o1TTKJy+DZ5nWgio4dHhYX/5+g7KipUmpB5PbtBCk10FULOZDJEopPHLz69Kc8Hq35LotJIUoHDZMs3cM2cmjLzyaL9v/1dmvZ2ZcpbYm5FgP7nITd+y8FvgdTrU5MqDyxIRVj3bLud2SfEYHuVPd/PMrGBu~1',
+#    'referer': 'https://accounts.rdstation.com.br/?affiliate_id=%28not%20set%29&email_signup_account=false&exist_account=false&gclid=%28not%20set%29&lmdsid=%28not%20set%29&locale=pt-BR&redirect_to=https%3A%2F%2Fapp.rdstation.com.br%2Fauth%2Fcallback&referrer=%28not%20set%29&show_signup_account=false&signup_final_step=false&trial_origin=%28not%20set%29&utm_campaign=%28not%20set%29&utm_medium=%28not%20set%29&utm_source=%28not%20set%29&utm_term=%28not%20set%29&xtra=%28not%20set%29',
+#    'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+#    'sec-ch-ua-mobile': '?0',
+#    'sec-ch-ua-platform': '"macOS"',
+#    'sec-fetch-dest': 'document',
+#    'sec-fetch-mode': 'navigate',
+#    'sec-fetch-site': 'same-site',
+#    'sec-fetch-user': '?1',
+#    'upgrade-insecure-requests': '1',
+#    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+#}
+#
+#params = {
+#    'segmentation_list': '6496060',
+#}
+#
+#response = requests.get('https://app.rdstation.com.br/leads', params=params, cookies=cookies, headers=headers)
+#
+#
+#soup = BeautifulSoup(response.content, 'html.parser')
+#
+#email_number = ""
+#
+#infos_selector = soup.find_all('div', class_='col-xs-5')
+#for info_selector in infos_selector:
+#    right_div = info_selector.find('span', class_='btn-height-sm')
+#    if right_div:
+#      email_info = right_div.get_text()
+#      emailNoSpape = email_info.split('\n          ')[1]
+#      email_number = emailNoSpape.split('.')[0]
+#      email_decimal = (email_info.split('.')[1])[0]
+#      email_count = email_number + "." + email_decimal + "K"
 
-cookies = {
-    '_gid': 'GA1.3.841158784.1657081595',
-    'NPS_efd9b1a6_last_seen': '1657081628591',
-    'intercom-id-hq3qq4eb': 'e5043f4d-96ed-4f4e-b632-499a74d25eb2',
-    'drift_aid': 'a0fe3ffd-3efa-4b45-8109-155e76b47eb0',
-    'driftt_aid': 'a0fe3ffd-3efa-4b45-8109-155e76b47eb0',
-    '_hjSessionUser_863034': 'eyJpZCI6ImE3NDdhZTkwLTc4MjgtNTUyZS04NGFjLWM4YTM2OTIyM2M3OSIsImNyZWF0ZWQiOjE2NTcwODE2Mjc3MjYsImV4aXN0aW5nIjp0cnVlfQ==',
-    '_hjSessionUser_2716062': 'eyJpZCI6IjY4MzkzNTZkLWYwYzItNWZlZi04NjA5LTE0ZDYyYjlmYzA4NSIsImNyZWF0ZWQiOjE2NTcwODE1OTU3NzAsImV4aXN0aW5nIjp0cnVlfQ==',
-    'split': '%7B%22xp_ab_test%22%3A%22variant%22%2C%22caterpie_activation_strategies%22%3A%22control%22%7D',
-    'ajs_group_id': '434927',
-    'ajs_user_id': '697398',
-    'ajs_anonymous_id': '2e1316f6-da5f-4f8b-bbd3-cdf64038e530',
-    'cto_bundle': 'GoVtFV9MS05nTzFUYVB4NmI5NFRWaU56Vk5oVXM5WDk1SlAlMkJGNWZwJTJCT2VCN2liSW85MlVMTGg3Ym5LdzRzNFVpOFFGcHJHQXlNRSUyRnNad3hZNFJmcDhMQTZpTDgxZ1NZOFZWejRrTlZLTkpPTG5BcVJSWDh0dmZyS29yaUc2Sk5DaFFNRXJHZ3ZFYlJieEZmUkwxR09abjN2VVJxTTVsZWp0ZnByMXozaE5mWG92NzAlM0Q',
-    '__rdsid': 'dbd63b9d3653e6d749a8d0342788ec9b',
-    'ak_bmsc': '9879A6F65AF2BF88FDF2EE932C34DC60~000000000000000000000000000000~YAAQT9PPFzjyYLOBAQAA1jK73BCBgxMGSDePWE4bdgAeFP8aDWEh7EzdtjOcm1XO7MFXNI77OZQ44o+Of92muQ5zZ0ZSC6Zh8zA4lR8O7GJ/TyFqmre1USae3YwEqO2o/ahn9WH8Nvw6xOiGnogDw/4/i3pv/qYXwTXqDN9CdLfrVjY9Kz+UBVZ41SuV2YgyeuBI0Rt3eA8p7kFToM54EGPfsijd7Bk3WbZeiLz9mFX5PpUhe1/eTqzN94sSnu8MHVicHtF9Luyx/pL3M5WM50xZxnVHMest/FH1Y6GYuikG7n1djgetR5690LaXAz66FTLGTlThdKbtteKoqbHKH9JBz2yqNEM3DmvafHMII2XAQBNv2Z7CVdaBpfRSxkjbhBS1CKpGKl4H4AqrBgSIPw==',
-    '_hjSession_2716062': 'eyJpZCI6ImQ2NjIzYmZmLWMzZDUtNDdiNi1iOWNlLTE3NGZjNTk5NzM2MiIsImNyZWF0ZWQiOjE2NTcyNjU2NjU0MzksImluU2FtcGxlIjp0cnVlfQ==',
-    '_hjAbsoluteSessionInProgress': '0',
-    '_gac_UA-17276574-1': '1.1657265666.%2528not%2520set%2529',
-    '_ga': 'GA1.1.1486625735.1657081595',
-    '_ga_GYGJG4ZD5K': 'GS1.1.1657265665.5.1.1657265697.0',
-    '_hjIncludedInSessionSample': '1',
-    '_hjSession_863034': 'eyJpZCI6IjJlNTMyNzcyLWM4MTctNDBiNC1hNmYxLTVmNDIyOGQzZmM4NyIsImNyZWF0ZWQiOjE2NTcyNjU2OTkyMjMsImluU2FtcGxlIjp0cnVlfQ==',
-    '_hjIncludedInPageviewSample': '1',
-    '_hjCachedUserAttributes': 'eyJhdHRyaWJ1dGVzIjp7IkNvdW50cnlfSVBzdGFjayI6IkJSIiwiYWNjb3VudF9pZCI6IjQzNDkyNyIsImNvdW50cnlfY29kZSI6IkJSIiwiZW1haWwiOiJuZXRvQHNwaW5vZmYuZGlnaXRhbCIsImxhbmd1YWdlIjoicHQtQlIiLCJwbGFuIjoiUHJvIiwicm9sZSI6IkFETUlOIn0sInVzZXJJZCI6IjY5NzM5OCJ9',
-    '_uetsid': 'e5ef2030fce311ecb53bedaab331166c',
-    '_uetvid': 'e5ef5cc0fce311ecac2e85d56bbe46fb',
-    'drift_campaign_refresh': '3a83bb17-6455-4761-8d48-b2e51098ac13',
-    'bm_sv': 'C1CBCB906BF052920F27BC6F9B5C68C4~YAAQT9PPF2b1YLOBAQAAF7u73BBSN7bkrVjpTjizurZ6Yiwsc3L/PvH5ScvVSBsO3TCnHE1Rbv7To6y8rmy1s6IYzMJNVxPn8txTMk5W5yWMPVRqvhEVno0nOlDx8mxkAllH1XmtoGpB9yKK449LXVDDhG1Bwc/Zp4XAnZ01cybbqVMLQAwzYfZXJ5DUksaOxowSxHyfcvH9wS4kXDllYAcrAmIAWliqGuPAcjw4chaQatogqzocpUXKoVqA4kIC+BSWTfX0~1',
-    'drift_eid': '697398',
-    'intercom-session-hq3qq4eb': 'dWIxTnQwblhEaGFPSUhlazdkTE01RnlTUURYc3RxV0hZeitKV1hSWElab3hweS9sWG9nYy9tMkppNWwwVmxoTC0tbDV4N0tadzd3V3lUN2x6V1duelJyQT09--2d45622c68f318f378ca58233278ce84b9e0f91e',
-    '_dd_s': 'rum=0&expire=1657266899054',
-}
-
-headers = {
-    'authority': 'app.rdstation.com.br',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    # Requests sorts cookies= alphabetically
-    # 'cookie': '_gid=GA1.3.841158784.1657081595; NPS_efd9b1a6_last_seen=1657081628591; intercom-id-hq3qq4eb=e5043f4d-96ed-4f4e-b632-499a74d25eb2; drift_aid=a0fe3ffd-3efa-4b45-8109-155e76b47eb0; driftt_aid=a0fe3ffd-3efa-4b45-8109-155e76b47eb0; _hjSessionUser_863034=eyJpZCI6ImE3NDdhZTkwLTc4MjgtNTUyZS04NGFjLWM4YTM2OTIyM2M3OSIsImNyZWF0ZWQiOjE2NTcwODE2Mjc3MjYsImV4aXN0aW5nIjp0cnVlfQ==; _hjSessionUser_2716062=eyJpZCI6IjY4MzkzNTZkLWYwYzItNWZlZi04NjA5LTE0ZDYyYjlmYzA4NSIsImNyZWF0ZWQiOjE2NTcwODE1OTU3NzAsImV4aXN0aW5nIjp0cnVlfQ==; split=%7B%22xp_ab_test%22%3A%22variant%22%2C%22caterpie_activation_strategies%22%3A%22control%22%7D; ajs_group_id=434927; ajs_user_id=697398; ajs_anonymous_id=2e1316f6-da5f-4f8b-bbd3-cdf64038e530; cto_bundle=GoVtFV9MS05nTzFUYVB4NmI5NFRWaU56Vk5oVXM5WDk1SlAlMkJGNWZwJTJCT2VCN2liSW85MlVMTGg3Ym5LdzRzNFVpOFFGcHJHQXlNRSUyRnNad3hZNFJmcDhMQTZpTDgxZ1NZOFZWejRrTlZLTkpPTG5BcVJSWDh0dmZyS29yaUc2Sk5DaFFNRXJHZ3ZFYlJieEZmUkwxR09abjN2VVJxTTVsZWp0ZnByMXozaE5mWG92NzAlM0Q; __rdsid=dbd63b9d3653e6d749a8d0342788ec9b; ak_bmsc=9879A6F65AF2BF88FDF2EE932C34DC60~000000000000000000000000000000~YAAQT9PPFzjyYLOBAQAA1jK73BCBgxMGSDePWE4bdgAeFP8aDWEh7EzdtjOcm1XO7MFXNI77OZQ44o+Of92muQ5zZ0ZSC6Zh8zA4lR8O7GJ/TyFqmre1USae3YwEqO2o/ahn9WH8Nvw6xOiGnogDw/4/i3pv/qYXwTXqDN9CdLfrVjY9Kz+UBVZ41SuV2YgyeuBI0Rt3eA8p7kFToM54EGPfsijd7Bk3WbZeiLz9mFX5PpUhe1/eTqzN94sSnu8MHVicHtF9Luyx/pL3M5WM50xZxnVHMest/FH1Y6GYuikG7n1djgetR5690LaXAz66FTLGTlThdKbtteKoqbHKH9JBz2yqNEM3DmvafHMII2XAQBNv2Z7CVdaBpfRSxkjbhBS1CKpGKl4H4AqrBgSIPw==; _hjSession_2716062=eyJpZCI6ImQ2NjIzYmZmLWMzZDUtNDdiNi1iOWNlLTE3NGZjNTk5NzM2MiIsImNyZWF0ZWQiOjE2NTcyNjU2NjU0MzksImluU2FtcGxlIjp0cnVlfQ==; _hjAbsoluteSessionInProgress=0; _gac_UA-17276574-1=1.1657265666.%2528not%2520set%2529; _ga=GA1.1.1486625735.1657081595; _ga_GYGJG4ZD5K=GS1.1.1657265665.5.1.1657265697.0; _hjIncludedInSessionSample=1; _hjSession_863034=eyJpZCI6IjJlNTMyNzcyLWM4MTctNDBiNC1hNmYxLTVmNDIyOGQzZmM4NyIsImNyZWF0ZWQiOjE2NTcyNjU2OTkyMjMsImluU2FtcGxlIjp0cnVlfQ==; _hjIncludedInPageviewSample=1; _hjCachedUserAttributes=eyJhdHRyaWJ1dGVzIjp7IkNvdW50cnlfSVBzdGFjayI6IkJSIiwiYWNjb3VudF9pZCI6IjQzNDkyNyIsImNvdW50cnlfY29kZSI6IkJSIiwiZW1haWwiOiJuZXRvQHNwaW5vZmYuZGlnaXRhbCIsImxhbmd1YWdlIjoicHQtQlIiLCJwbGFuIjoiUHJvIiwicm9sZSI6IkFETUlOIn0sInVzZXJJZCI6IjY5NzM5OCJ9; _uetsid=e5ef2030fce311ecb53bedaab331166c; _uetvid=e5ef5cc0fce311ecac2e85d56bbe46fb; drift_campaign_refresh=3a83bb17-6455-4761-8d48-b2e51098ac13; bm_sv=C1CBCB906BF052920F27BC6F9B5C68C4~YAAQT9PPF2b1YLOBAQAAF7u73BBSN7bkrVjpTjizurZ6Yiwsc3L/PvH5ScvVSBsO3TCnHE1Rbv7To6y8rmy1s6IYzMJNVxPn8txTMk5W5yWMPVRqvhEVno0nOlDx8mxkAllH1XmtoGpB9yKK449LXVDDhG1Bwc/Zp4XAnZ01cybbqVMLQAwzYfZXJ5DUksaOxowSxHyfcvH9wS4kXDllYAcrAmIAWliqGuPAcjw4chaQatogqzocpUXKoVqA4kIC+BSWTfX0~1; drift_eid=697398; intercom-session-hq3qq4eb=dWIxTnQwblhEaGFPSUhlazdkTE01RnlTUURYc3RxV0hZeitKV1hSWElab3hweS9sWG9nYy9tMkppNWwwVmxoTC0tbDV4N0tadzd3V3lUN2x6V1duelJyQT09--2d45622c68f318f378ca58233278ce84b9e0f91e; _dd_s=rum=0&expire=1657266899054',
-    'if-none-match': 'W/"91f2e3765aa2e3b07ac1d2c2826e1d1c"',
-    'referer': 'https://accounts.rdstation.com.br/?affiliate_id=%28not%20set%29&email_signup_account=false&exist_account=false&gclid=%28not%20set%29&lmdsid=%28not%20set%29&locale=pt-BR&redirect_to=https%3A%2F%2Fapp.rdstation.com.br%2Fauth%2Fcallback&referrer=%28not%20set%29&show_signup_account=false&signup_final_step=false&trial_origin=%28not%20set%29&utm_campaign=%28not%20set%29&utm_medium=%28not%20set%29&utm_source=%28not%20set%29&utm_term=%28not%20set%29&xtra=%28not%20set%29',
-    'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-site',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-}
-
-params = {
-    'segmentation_list': '6496060',
-}
-
-response = requests.get('https://app.rdstation.com.br/leads', params=params, cookies=cookies, headers=headers)
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless = True, slow_mo=50)
+    page = browser.new_page()
+    page.goto('https://app.rdstation.com.br/leads?segmentation_list=6496060')
+    page.fill('input#email','neto@spinoff.digital')
+    page.fill('input#password','061231(RDStation)')
+    page.click('button[type=submit]')
+    page.is_visible('div.leads_crm')
+    html = page.inner_html('#content')
+    soup = BeautifulSoup(html,'html.parser')   
+    infos_selector = soup.find_all('div', class_='col-xs-5')
+    for info_selector in infos_selector:
+        right_div = info_selector.find('span', class_='btn-height-sm')
+        if right_div:
+            email_info = right_div.get_text()
+            emailNoSpape = email_info.split('\n          ')[1]
+            email_number = emailNoSpape.split('.')[0]
+            email_decimal = (email_info.split('.')[1])[0]
+            email_count = email_number + "." + email_decimal + "K"
+            print (email_count)
 
 
-soup = BeautifulSoup(response.content, 'html.parser')
 
-email_number = ""
-
-infos_selector = soup.find_all('div', class_='col-xs-5')
-for info_selector in infos_selector:
-    right_div = info_selector.find('span', class_='btn-height-sm')
-    if right_div:
-      email_info = right_div.get_text()
-      emailNoSpape = email_info.split('\n          ')[1]
-      email_number = emailNoSpape.split('.')[0]
-      email_decimal = (email_info.split('.')[1])[0]
-      email_count = email_number + "." + email_decimal + "K"
 
 #YouTube
 driver.get('https://www.youtube.com/c/SnapdragonBrasil')
