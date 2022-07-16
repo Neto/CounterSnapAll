eb93f6c683833460433584ae73de7dbc12c81d81
Mentor Neto <neto@spinoff.digital>
2022-07-15 22:17:33 -0300

    commit to 'master'

--- getGraphic.py
+++ getGraphic.py
@@ -36,7 +36,6 @@ for item in all_stats:
 
 grapf1 = pd.DataFrame.from_dict(records)
 
-grTot = grapf1.total
 grEm = grapf1.emailC
 grYB = grapf1.youtube
 grYL = grapf1.youtubeL
@@ -44,19 +43,142 @@ grTB = grapf1.twitter
 grTL = grapf1.twitterL
 grIB = grapf1.instagram
 grIL = grapf1.instagramL
+grT = grapf1.total
 
-plt.plot(grapf1.date,grapf1.grYB)
-plt.plot(grapf1.date,grapf1.grYL)
-plt.plot(grapf1.date,grapf1.grTB)
-plt.plot(grapf1.date,grapf1.grTL)
-plt.plot(grapf1.date,grapf1.grIB)
-plt.plot(grapf1.date,grapf1.grIL)
-plt.plot(grapf1.date,grTot)
+# EMAIL
+plt.clf()
+plt.title("Snapdragon Followers Email - Brazil")
+plt.plot(grapf1.date,grapf1.emailC)
+ax = plt.gca()
+every_nth = 2
+for n, label in enumerate(ax.yaxis.get_ticklabels()):
+    if n % every_nth != 0:
+        label.set_visible(False)
+every_nth2 = 5
+for n, label in enumerate(ax.xaxis.get_ticklabels()):
+    if n % every_nth2 != 0:
+        label.set_visible(False)
+ax.invert_xaxis()
+ax.invert_yaxis()
+ax.set_facecolor('white')
+plt.savefig('/Library/WebServer/Documents/Spinoff/graphicEm.png')
+
+# YOUTUBE BR
+plt.clf()
+plt.title("Snapdragon Followers YouTube - Brazil")
+plt.plot(grapf1.date,grYB)
+ax = plt.gca()
+every_nth = 2
+for n, label in enumerate(ax.yaxis.get_ticklabels()):
+    if n % every_nth != 0:
+        label.set_visible(False)
+every_nth2 = 5
+for n, label in enumerate(ax.xaxis.get_ticklabels()):
+    if n % every_nth2 != 0:
+        label.set_visible(False)
+ax.invert_xaxis()
+ax.invert_yaxis()
+ax.set_facecolor('white')
+plt.savefig('/Library/WebServer/Documents/Spinoff/graphicYB.png')
+
+# YOUTUBE LT
+plt.clf()
+plt.title("Snapdragon Followers Youtube - Latam")
+plt.plot(grapf1.date,grYL)
+ax = plt.gca()
+every_nth = 2
+for n, label in enumerate(ax.yaxis.get_ticklabels()):
+    if n % every_nth != 0:
+        label.set_visible(False)
+every_nth2 = 5
+for n, label in enumerate(ax.xaxis.get_ticklabels()):
+    if n % every_nth2 != 0:
+        label.set_visible(False)
+ax.invert_xaxis()
+ax.invert_yaxis()
+ax.set_facecolor('white')
+plt.savefig('/Library/WebServer/Documents/Spinoff/graphicYL.png')
+
+
+
+# TWITTER BR
+plt.clf()
+plt.title("Snapdragon Followers Twitter - Brazil")
+plt.plot(grapf1.date,grTB)
+ax = plt.gca()
+every_nth = 2
+for n, label in enumerate(ax.yaxis.get_ticklabels()):
+    if n % every_nth != 0:
+        label.set_visible(False)
+every_nth2 = 5
+for n, label in enumerate(ax.xaxis.get_ticklabels()):
+    if n % every_nth2 != 0:
+        label.set_visible(False)
+ax.invert_xaxis()
+ax.invert_yaxis()
+ax.set_facecolor('white')
+plt.savefig('/Library/WebServer/Documents/Spinoff/graphicTB.png')
+
+# EMAIL
+plt.clf()
+plt.title("Snapdragon Followers Twitter - Latam")
+plt.plot(grapf1.date,grTL)
+ax = plt.gca()
+every_nth = 2
+for n, label in enumerate(ax.yaxis.get_ticklabels()):
+    if n % every_nth != 0:
+        label.set_visible(False)
+every_nth2 = 5
+for n, label in enumerate(ax.xaxis.get_ticklabels()):
+    if n % every_nth2 != 0:
+        label.set_visible(False)
+ax.invert_xaxis()
+ax.invert_yaxis()
+ax.set_facecolor('white')
+plt.savefig('/Library/WebServer/Documents/Spinoff/graphicTL.png')
+
+# INSTAGRAM BR
+plt.clf()
+plt.title("Snapdragon Followers Instagram - Brazil")
+plt.plot(grapf1.date,grIB)
+ax = plt.gca()
+every_nth = 2
+for n, label in enumerate(ax.yaxis.get_ticklabels()):
+    if n % every_nth != 0:
+        label.set_visible(False)
+every_nth2 = 5
+for n, label in enumerate(ax.xaxis.get_ticklabels()):
+    if n % every_nth2 != 0:
+        label.set_visible(False)
+ax.invert_xaxis()
+ax.invert_yaxis()
+ax.set_facecolor('white')
+plt.savefig('/Library/WebServer/Documents/Spinoff/graphicIB.png')
+
+# INSTAGRAM LT
+plt.clf()
+plt.title("Snapdragon Followers Instagram - Latam")
+plt.plot(grapf1.date,grIL)
+ax = plt.gca()
+every_nth = 2
+for n, label in enumerate(ax.yaxis.get_ticklabels()):
+    if n % every_nth != 0:
+        label.set_visible(False)
+every_nth2 = 5
+for n, label in enumerate(ax.xaxis.get_ticklabels()):
+    if n % every_nth2 != 0:
+        label.set_visible(False)
+ax.invert_xaxis()
+ax.invert_yaxis()
+ax.set_facecolor('white')
+plt.savefig('/Library/WebServer/Documents/Spinoff/graphicIL.png')
 
 
 # TOTAL
+plt.clf()
+plt.title("Snapdragon Followers Total - Brazil & Latam")
+plt.plot(grapf1.date,grT)
 ax = plt.gca()
-plt.title("Snapdragon Followers TOTAL - Brazil & Latam")
 every_nth = 2
 for n, label in enumerate(ax.yaxis.get_ticklabels()):
     if n % every_nth != 0:
@@ -69,132 +191,6 @@ ax.invert_xaxis()
 ax.invert_yaxis()
 ax.set_facecolor('white')
 plt.savefig('/Library/WebServer/Documents/Spinoff/graphicT.png')
-
-# EMAIL
-plt.clf()
-plt.title("Snapdragon Followers Email - Brazil")
-plt.plot(grapf1.date,grapf1.emailC)
-ax = plt.gca()
-every_nth = 2
-for n, label in enumerate(ax.yaxis.get_ticklabels()):
-    if n % every_nth != 0:
-        label.set_visible(False)
-every_nth2 = 5
-for n, label in enumerate(ax.xaxis.get_ticklabels()):
-    if n % every_nth2 != 0:
-        label.set_visible(False)
-ax.invert_xaxis()
-ax.invert_yaxis()
-ax.set_facecolor('white')
-plt.savefig('/Library/WebServer/Documents/Spinoff/graphicEm.png')
-
-# EMAIL
-plt.clf()
-plt.title("Snapdragon Followers Email - Brazil")
-plt.plot(grapf1.date,grapf1.emailC)
-ax = plt.gca()
-every_nth = 2
-for n, label in enumerate(ax.yaxis.get_ticklabels()):
-    if n % every_nth != 0:
-        label.set_visible(False)
-every_nth2 = 5
-for n, label in enumerate(ax.xaxis.get_ticklabels()):
-    if n % every_nth2 != 0:
-        label.set_visible(False)
-ax.invert_xaxis()
-ax.invert_yaxis()
-ax.set_facecolor('white')
-plt.savefig('/Library/WebServer/Documents/Spinoff/graphicEm.png')
-
-# EMAIL
-plt.clf()
-plt.title("Snapdragon Followers Email - Brazil")
-plt.plot(grapf1.date,grapf1.emailC)
-ax = plt.gca()
-every_nth = 2
-for n, label in enumerate(ax.yaxis.get_ticklabels()):
-    if n % every_nth != 0:
-        label.set_visible(False)
-every_nth2 = 5
-for n, label in enumerate(ax.xaxis.get_ticklabels()):
-    if n % every_nth2 != 0:
-        label.set_visible(False)
-ax.invert_xaxis()
-ax.invert_yaxis()
-ax.set_facecolor('white')
-plt.savefig('/Library/WebServer/Documents/Spinoff/graphicEm.png')
-
-# EMAIL
-plt.clf()
-plt.title("Snapdragon Followers Email - Brazil")
-plt.plot(grapf1.date,grapf1.emailC)
-ax = plt.gca()
-every_nth = 2
-for n, label in enumerate(ax.yaxis.get_ticklabels()):
-    if n % every_nth != 0:
-        label.set_visible(False)
-every_nth2 = 5
-for n, label in enumerate(ax.xaxis.get_ticklabels()):
-    if n % every_nth2 != 0:
-        label.set_visible(False)
-ax.invert_xaxis()
-ax.invert_yaxis()
-ax.set_facecolor('white')
-plt.savefig('/Library/WebServer/Documents/Spinoff/graphicEm.png')
-
-# EMAIL
-plt.clf()
-plt.title("Snapdragon Followers Email - Brazil")
-plt.plot(grapf1.date,grapf1.emailC)
-ax = plt.gca()
-every_nth = 2
-for n, label in enumerate(ax.yaxis.get_ticklabels()):
-    if n % every_nth != 0:
-        label.set_visible(False)
-every_nth2 = 5
-for n, label in enumerate(ax.xaxis.get_ticklabels()):
-    if n % every_nth2 != 0:
-        label.set_visible(False)
-ax.invert_xaxis()
-ax.invert_yaxis()
-ax.set_facecolor('white')
-plt.savefig('/Library/WebServer/Documents/Spinoff/graphicEm.png')
-
-# EMAIL
-plt.clf()
-plt.title("Snapdragon Followers Email - Brazil")
-plt.plot(grapf1.date,grapf1.emailC)
-ax = plt.gca()
-every_nth = 2
-for n, label in enumerate(ax.yaxis.get_ticklabels()):
-    if n % every_nth != 0:
-        label.set_visible(False)
-every_nth2 = 5
-for n, label in enumerate(ax.xaxis.get_ticklabels()):
-    if n % every_nth2 != 0:
-        label.set_visible(False)
-ax.invert_xaxis()
-ax.invert_yaxis()
-ax.set_facecolor('white')
-plt.savefig('/Library/WebServer/Documents/Spinoff/graphicEm.png')
-
-# EMAIL
-plt.clf()
-plt.title("Snapdragon Followers Email - Brazil")
-plt.plot(grapf1.date,grapf1.emailC)
-ax = plt.gca()
-every_nth = 2
-for n, label in enumerate(ax.yaxis.get_ticklabels()):
-    if n % every_nth != 0:
-        label.set_visible(False)
-every_nth2 = 5
-for n, label in enumerate(ax.xaxis.get_ticklabels()):
-    if n % every_nth2 != 0:
-        label.set_visible(False)
-ax.invert_xaxis()
-ax.invert_yaxis()
-ax.set_facecolor('white')
-plt.savefig('/Library/WebServer/Documents/Spinoff/graphicEm.png')
 
 
 #plt.show()
