3431fb60ecdbe45d6ab4ccd2b375ddfd0692c395
Mentor Neto <neto@spinoff.digital>
2023-04-20 02:07:09 -0300

    main

---
+++ run_CounterSnapAll.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /Users/neto/ARDUINO/CounterSnapAll
+nohup flask run --host=0.0.0 --port=8080
+
+$SHELL
--- run_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-tmux new -s Control
-flask run --host=0.0.0 --port=8080
